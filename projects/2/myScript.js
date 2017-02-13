36b0b14be68ca5aaeac49b686d9a9267dfdc4527
Edward Hughes <ehughe5@uic.edu>
2017-02-13 02:18:36 +0000

    c

--- projects/2/myScript.js
+++ projects/2/myScript.js
@@ -1,5 +1,6 @@
 
 $(function () {
+     console.log("asdsdasd");
      $(document).ajax({
           url: "https://data.cityofchicago.org/resource/7as2-ds3y.json?$group=community_area&$order=community_area%20asc&$where=status=%22open%22&$select=community_area,%20count(*)",
           type: "GET",
